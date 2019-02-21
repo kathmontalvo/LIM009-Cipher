7c2806d5c5408dcaf69b9198fd96941e03611449
kathmontalvo <kathmontalvo123@gmail.com>
2019-02-21 10:50:09 -0500

    testeando funcion descifrar

--- src/cipher.js
+++ src/cipher.js
@@ -1,11 +1,18 @@
-/*window.cipher = {
-  encode: (offset,string) => {
-    //const vocabulary = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    //vocabulary.charCodeAt(keyUp)
-    
-  }
+window.cipher = {
+ encode: (offset,string) => {
+    const r= string.charCodeAt();
+    if (r > 64 && r < 91){
+    const formu = String.fromCharCode((r-65+offset)%26+65);
+    decodeMsg.innerHTML+=formu
+    }
+  },
 
   decode: (offset,string) => {
+    const d = string.charCodeAt()
+    if (d > 64 && d < 91){
+    const formula = String.fromCharCode((d+65-offset)%26+65);
+    encodeMsg.innerHTML+=formula
+
+    }
   }
-}}
-*/
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 const encodeMsg = document.getElementById("encodeMsg");
 const decodeMsg = document.getElementById("decodeMsg");
 const desp = document.getElementById("offsetNumber");
-const btn = document.getElementById("btn");
+// const btn = document.getElementById("btn");
 
-btn.addEventListener('click', ()=>{
+encodeMsg.addEventListener('keyup', ()=>{
     const cipherText = encodeMsg.value; 
     for ( i = 0; i < cipherText.length; i++) {
     const x = cipherText[i];
-    const a = parseInt(desp.value);
-    const r= x.charCodeAt();
-    if (r > 64 && r < 91){
-    const formu = String.fromCharCode((r-65+a)%26+65);
-    decodeMsg.innerHTML+=formu
+    a=desp.value
+    cipher.encode(a,x) //función en cipher.js
     }
-}
+})
+
+decodeMsg.addEventListener('keyup', ()=>{
+    const dcipherText = decodeMsg.value;
+    for (i=0 ; i<dcipherText.length ; i++){
+    const y = dcipherText[i];
+    a=desp.value
+    cipher.decode(a,y) 
+    }
 })
