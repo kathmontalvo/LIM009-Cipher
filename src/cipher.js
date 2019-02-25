aee74d800394322290658d99dad038db39f2ed23
kathmontalvo <kathmontalvo123@gmail.com>
2019-02-25 13:01:57 -0500

    diseño y descif minusculas

--- src/cipher.js
+++ src/cipher.js
@@ -12,10 +12,12 @@ window.cipher = {
   decode: (offset,string, arr, inic) => {
     const d = string.charCodeAt()
     
-    if (d >= inic && d <=inic+25){
-    const formulaD = String.fromCharCode((d+inic-offset)%26+  inic);
+    if (d >= inic && d <=inic+25 && inic==65){
+    const formulaD = String.fromCharCode((d+inic-offset)%26+ inic);
     arr.push(formulaD)
-
-    }
-  }  
+    }  else if (d >= inic && d <=inic+25 && inic==97){
+      const formulaD = String.fromCharCode((d+(inic-65+1)-offset)% 26 + inic);
+      arr.push(formulaD)
+      } 
+  }
 }
